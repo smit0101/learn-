cd7a8ab63ae084ae4eff5f2f988c89e03db870e5
smit0101 <smitkalkani9099@gmail.com>
2022-05-20 01:58:14 +0530

    Initiated Project

--- src/jvmMain/kotlin/ImageStuff.kt
+++ src/jvmMain/kotlin/ImageStuff.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 data class ImageData(val success:Boolean, val data:Data)
 
 @Serializable
-data class Memes(val id:Int, val name:String?, val url:String, val width:Int, val height:Int, @SerialName("box_count") val boxCount:Int)
+data class Memes(val id:Int, val name:String, val url:String, val width:Int, val height:Int, @SerialName("box_count") val boxCount:Int)
 
 @Serializable
 data class Data(val memes:MutableList<Memes>)
